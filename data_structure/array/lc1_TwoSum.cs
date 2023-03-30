80226291ab45abf13fad1b5394ff3b0b1e07733d
HinataUp <19965331661@163.com>
2023-03-30 10:29:35 +0800

    入门 两数之和

---
+++ data_structure/array/lc1_TwoSum.cs
@@ -0,0 +1,16 @@
+﻿namespace code.data_structure.array; 
+
+public class lc1_TwoSum {
+    public int[] TwoSum(int[] nums, int target) {
+        int [] res = new int[2];
+        for( int i = 0; i < nums.Length - 1; i++){
+            for (int j = i +1 ;j < nums.Length; j++){
+                if(nums[i] + nums[j] == target){
+                    res[0] = i;
+                    res[1] = j;
+                }
+            }
+        }
+        return res;
+    }
+}
