a69e01b7d7d20a8c2340183be56f617364c0e26b
HinataUp <19965331661@163.com>
2023-04-05 19:29:33 +0800

    相同的树

---
+++ data_structure/tree/lc100. Same Tree.cs
@@ -0,0 +1,11 @@
+﻿namespace code.data_structure.tree; 
+
+public class lc100__Same_Tree {
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+    }
+
+}
