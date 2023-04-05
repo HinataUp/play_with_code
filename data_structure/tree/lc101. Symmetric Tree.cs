8d76d2d04a3979ba4273e6421bf6b9a03f267e15
HinataUp <19965331661@163.com>
2023-04-05 19:16:05 +0800

    对称二叉树

---
+++ data_structure/tree/lc101. Symmetric Tree.cs
@@ -0,0 +1,16 @@
+﻿namespace code.data_structure.tree;
+
+public class lc101__Symmetric_Tree {
+    public bool IsSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return Symmetric(root.left, root.right);
+    }
+
+    private bool Symmetric(TreeNode rootLeft, TreeNode rootRight) {
+        if (rootLeft == null && rootRight == null) return true;
+        if (rootLeft == null || rootRight == null) return false;
+        if (rootLeft.val != rootRight.val) return false;
+        return Symmetric(rootLeft.left, rootRight.right) &&
+               Symmetric(rootLeft.right, rootRight.left);
+    }
+}
