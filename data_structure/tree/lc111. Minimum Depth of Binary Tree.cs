3b320c316ec1dfdc6054836e3bf83b5ba08753f0
HinataUp <19965331661@163.com>
2023-04-05 19:44:58 +0800

    二叉树的最小深度，和最大深度仅一行代码之差

---
+++ data_structure/tree/lc111. Minimum Depth of Binary Tree.cs
@@ -0,0 +1,25 @@
+﻿namespace code.data_structure.tree;
+
+public class lc111__Minimum_Depth_of_Binary_Tree {
+    public int MinDepth(TreeNode root) { 
+        if (root == null) return 0;
+        int depth = 0;
+        Queue<TreeNode> queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+        while (queue.Count != 0) {
+            int size = queue.Count;
+            while (size > 0) {
+                var node = queue.Dequeue();
+                // 只要某个节点的 双子同时不存在就直接返回
+                if (node.left == null && node.right == null) return depth + 1;
+                if (node.left != null) queue.Enqueue(node.left);
+                if (node.right != null) queue.Enqueue(node.right);
+                size--;
+            }
+
+            depth++;
+        }
+
+        return depth;
+    }
+}
