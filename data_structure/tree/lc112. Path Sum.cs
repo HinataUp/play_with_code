e3e10a08d29d0202d855279860666f96f03c5c88
HinataUp <19965331661@163.com>
2023-04-07 21:14:56 +0800

    路径总和

---
+++ data_structure/tree/lc112. Path Sum.cs
@@ -0,0 +1,10 @@
+﻿namespace code.data_structure.tree; 
+
+public class lc112__Path_Sum {
+    public bool HasPathSum(TreeNode root, int targetSum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return targetSum == root.val;
+        return HasPathSum(root.left, targetSum - root.val) ||
+               HasPathSum(root.right, targetSum - root.val);
+    }
+}
