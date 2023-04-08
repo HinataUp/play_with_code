37ac278041acaa1adde80af328f3a5bc9e161647
HinataUp <19965331661@163.com>
2023-04-08 15:04:26 +0800

    2的幂

---
+++ algorithm/recursion/lc231. Power of Two.cs
@@ -0,0 +1,10 @@
+﻿namespace code.algorithm.recursion; 
+
+public class lc231__Power_of_Two {
+    public bool IsPowerOfTwo(int n) {
+        if (n == 0) return false;
+        if (n == 1) return true;
+        if (n % 2 == 1) return false;
+        return IsPowerOfTwo(n / 2);
+    }
+}
