62a00753cc19d4167b820e5e5aadc7a198ba61f0
HinataUp <19965331661@163.com>
2023-04-08 15:57:21 +0800

    从尾部到头部打印链表

---
+++ algorithm/recursion/lcoffer06ReversePrint.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+namespace code.algorithm.recursion;
+
+// 从尾部到头部打印链表
+public class lcoffer06ReversePrint {
+    public int[] ReversePrint(ListNode head) {
+        if (head == null) return new int[0];
+        List<int> res = new List<int>();
+
+        ListNode Rev(ListNode node) {
+            if (node == null) return null;
+            res.Add(node.val);
+            return Rev(node.next);
+        }
+
+        Rev(head);
+        return res.ToArray();
+    }
+}
