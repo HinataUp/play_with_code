43e8ea16591a45f71fc28b89c4a2ea674ebcd18d
HinataUp <19965331661@163.com>
2023-04-08 08:11:04 +0800

    21 合并两个升序 链表

---
+++ algorithm/recursion/lc21. Merge Two Sorted Lists.cs
@@ -0,0 +1,27 @@
+﻿namespace code.algorithm.recursion;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// 合并两个升序 链表，返回合并后的链表
+// 递归实现
+public class lc21__Merge_Two_Sorted_Lists {
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        if (list1.val < list2.val) {
+            list1.next = MergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = MergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
