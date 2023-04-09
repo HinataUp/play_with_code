f82383d869dd7e24b5d1d487b55fab1ec64320cc
HinataUp <19965331661@163.com>
2023-04-09 19:35:08 +0800

    修改 反向打印链表 注意 递归和add时机 有讲究

--- algorithm/recursion/lcoffer06ReversePrint.cs
+++ algorithm/recursion/lcoffer06ReversePrint.cs
@@ -6,15 +6,35 @@ namespace code.algorithm.recursion;
 public class lcoffer06ReversePrint {
     public int[] ReversePrint(ListNode head) {
         if (head == null) return new int[0];
-        List<int> res = new List<int>();
-
-        ListNode Rev(ListNode node) {
-            if (node == null) return null;
-            res.Add(node.val);
-            return Rev(node.next);
+        var count = 0;
+        var node = head;
+        while (node != null) {
+            count++;
+            node = node.next;
         }
 
-        Rev(head);
+        var result = new int[count];
+        int i = count - 1;
+        while (0 <= i) {
+            result[i--] = head.val;
+            head = head.next;
+        }
+
+        return result;
+    }
+
+    // 改为递归
+    public int[] ReversePrint2(ListNode head) {
+        List<int> res = new List<int>();
+        ListNode dummy = new ListNode(-1, head);
+        reversprint(dummy.next, res);
+
+        void reversprint(ListNode node, List<int> res) {
+            if (node == null) return;
+            reversprint(node.next, res);
+            res.Add(node.val);
+        }
         return res.ToArray();
     }
+
 }
