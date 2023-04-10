3ed5c2421e28954a8c17b977bf7fdb9aed12205e
HinataUp <19965331661@163.com>
2023-04-10 08:29:06 +0800

    3的幂

---
+++ algorithm/recursion/lc326. Power of Three.cs
@@ -0,0 +1,12 @@
+﻿namespace code.algorithm.recursion;
+
+public class lc326__Power_of_Three {
+    public bool IsPowerOfThree(int n) {
+        // 幂运算 判断是不是3的k次方 有值 1 3 9 27...
+
+        if (n == 0) return false;
+        if (n == 1) return true;
+        if (n % 3 == 0) return IsPowerOfThree(n / 3);
+        return false;
+    }
+}
