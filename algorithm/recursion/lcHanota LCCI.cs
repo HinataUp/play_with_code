d3715a7fe7cd3282984fce3eb6c262dc0ef609fa
HinataUp <19965331661@163.com>
2023-04-11 19:26:24 +0800

    汉诺塔TODO 理解

---
+++ algorithm/recursion/lcHanota LCCI.cs
@@ -0,0 +1,20 @@
+﻿namespace code.algorithm.recursion;
+
+public class lcHanota_LCCI {
+    public void Hanota(IList<int> A, IList<int> B, IList<int> C) {
+        int len = A.Count;
+        Move(len, A, B, C);
+
+        void Move(int len, IList<int> A, IList<int> B, IList<int> C) {
+            if (len == 1) {
+                C.Add(A[^1]); //  A[A.Count - 1] 等价
+                A.RemoveAt(A.Count - 1);
+                return;
+            }
+
+            Move(len - 1, A, C, B);
+            Move(1, A, B, C);
+            Move(len - 1, B, A, C);
+        }
+    }
+}
